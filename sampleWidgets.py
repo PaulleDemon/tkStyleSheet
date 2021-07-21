ed46f712ab027b7aa12200a79cfa83b2d937d663
Paul <paulsajo0@gmail.com>
2021-07-21 18:52:34 +0530

    added sampleWidgets.py

---
+++ sampleWidgets.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+
+
+def initLeftFrame():
+    tk.Label(left_frame, text="left Frame").pack()
+
+    left_lblFrame = tk.LabelFrame(left_frame, text="This is a label frame")
+    left_lblFrame.pack(fill="both", expand=True)
+
+    ent_lbl_frame = tk.Frame(left_lblFrame)
+    ent_lbl_frame.pack()
+
+    tk.Label(ent_lbl_frame, text="Entry").pack(side='left')
+    tk.Entry(ent_lbl_frame).pack(side='left', fill='x', expand=True)
+
+    var = tk.StringVar()
+    var.set("option1")
+    options = ["option1", "option2", "option3"]
+    tk.OptionMenu(left_lblFrame, var, *options).pack()
+
+    tk.Spinbox(left_lblFrame, from_=0, to=25).pack()
+    tk.Scale(left_lblFrame, variable=tk.IntVar(), from_=1, to=50, orient='horizontal').pack()
+
+
+def initRightFrame():
+    tk.Label(right_frame, text="right Frame").pack()
+    canvas = tk.Canvas(right_frame, height=50)
+    canvas.create_text(50, 20, text="This is a canvas")
+    canvas.pack()
+    tk.Text(right_frame, height=10).pack()
+
+    scrollbar = tk.Scrollbar(right_frame)
+    scrollbar.pack(side='right', fill='y')
+
+    mylist = tk.Listbox(right_frame, yscrollcommand=scrollbar.set)
+    for line in range(100):
+        mylist.insert('end', "This is line number " + str(line))
+
+    mylist.pack(side='left', fill='both', expand=True)
+    scrollbar.config(command=mylist.yview)
+
+
+root = tk.Tk()
+
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+file = tk.Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="File", menu=file)
+
+file.add_command(label="New", command=None)
+file.add_command(label="Open", command=None)
+
+paned_window = tk.PanedWindow(root, handlesize=4, orient=tk.HORIZONTAL)
+paned_window.pack(expand=True, fill="both")
+
+left_frame = tk.Frame(paned_window)
+right_frame = tk.Frame(paned_window)
+
+paned_window.add(left_frame, width=200)
+paned_window.add(right_frame)
+
+initLeftFrame()
+initRightFrame()
+
+root.mainloop()
